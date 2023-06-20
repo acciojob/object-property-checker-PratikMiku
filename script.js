c21b2d969f7d6191619b5ea1488fa16503fcae67
PRATIK KUMAR BISWAL <117885087+PratikMiku@users.noreply.github.com>
2023-06-20 09:28:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-  if(key===(sampleObject.red||sampleObject.green||sampleObject.white)){
-    console.log("true")
+  if(key==="red"){
+    console.log("true")  
+  }
+   if(key==="green"){
+    console.log("true")  
+  }
+   if(key==="white"){
+    console.log("true")  
   }
   else{
       console.log("false")
