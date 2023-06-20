94a5733b5ff15edbc4aeec08eb13b4fa046f3432
PRATIK KUMAR BISWAL <117885087+PratikMiku@users.noreply.github.com>
2023-06-20 09:32:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
-  if(key==="red"){
-    console.log("true")  
-  }
-   if(key==="green"){
-    console.log("true")  
-  }
-   if(key==="white"){
-    console.log("true")  
-  }
-  else{
-      console.log("false")
-  }
-}
-
+ return key in sampleObject;}
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
